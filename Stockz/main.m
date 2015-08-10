ff58410e4050b673c83527695b15489fd7f4b9f8
cwnidog <cwnidog@comcast.net>
2015-08-10 16:17:24 -0700

    Adding core to read and write values to a plist file

--- Stockz/main.m
+++ Stockz/main.m
@@ -8,10 +8,31 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-  @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
-  }
+int main(int argc, const char *argv[])
+{
+  @autoreleasepool
+  {
+    NSMutableArray *stocks = [NSMutableArray new];
+    NSMutableDictionary *stock;
+    
+    stock = [NSMutableDictionary dictionary];
+    [stock setObject:@"AAPL" forKey:@"symbol"];
+    [stock setObject:[NSNumber numberWithInt:200] forKey:@"shares"];
+    [stocks addObject:stock];
+    
+    stock = [NSMutableDictionary dictionary];
+    [stock setObject:@"GOOG" forKey:@"symbol"];
+    [stock setObject:[NSNumber numberWithInt:160] forKey:@"shares"];
+    [stocks addObject:stock];
+    
+    [stocks writeToFile:@"/tmp/stocks.plist" atomically:YES];
+    
+    NSArray *stockList = [NSArray arrayWithContentsOfFile:@"/tmp/stocks.plist"];
+    
+    for (NSDictionary *d in stockList)
+    {
+      NSLog(@"I have %@ shares of %@", [d objectForKey:@"shares"], [d objectForKey:@"symbol"]);
+    }
+  } // @autoreleasepool
     return 0;
-}
+} // main()
